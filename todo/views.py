5aa6b89d1c7a61cbb7eb964cbd565925833df018
cshunyee <cshunyee97@gmail.com>
2020-07-11 16:13:40 +0800

    completed all th function and styling

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 #Home page
 def home(request):
@@ -41,6 +43,7 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
@@ -48,6 +51,7 @@ def logoutuser(request):
 
 
 #Create data
+@login_required
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -62,23 +66,54 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error':'Bad data passed in'})
 
 #Read data
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html',{'todos':todos})
 
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos':todos})
+
+#Read and update data
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     form = TodoForm(instance=todo)
     if request.method == 'GET':
         return render(request, 'todo/viewtodo.html', {'todo': todo, 'form':form})
     else:
+        return render(request, 'todo/edittodo.html', {'todo': todo, 'form':form})
+
+# Update data
+@login_required
+def edittodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    form = TodoForm(instance=todo)
+    if request.method == 'GET':
+        return render(request, 'todo/edittodo.html', {'todo': todo, 'form':form})
+    else:
         try:
             form = TodoForm(request.POST, instance=todo)
             form.save()
-            return redirect('currenttodos')
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form':form})
+
         except ValueError:
             return render(request, 'todo/currenttodos.html', {'form': TodoForm(), 'error':'Bad data passed in'})
 
-#Update data
+@login_required
+def completetodo(request, todo_pk):
+    if request.method == 'POST':
+        todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
 
 #Delete data
+@login_required
+def deletetodo(request, todo_pk):
+    if request.method == 'POST':
+        todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+        todo.delete()
+        return redirect('currenttodos')
